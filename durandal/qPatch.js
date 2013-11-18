95780c2a486878b9f1898c1217bf7a83cf721c91
Timothy Moran <tyrsius@gmail.com>
2013-11-17 21:53:57 -0800

    Create qPatch.js

---
+++ durandal/qPatch.js
@@ -0,0 +1,36 @@
+define(['durandal/system', 'durandal/app'], function(system, app) {
+    
+    var install = function (config) {
+    	var config = config || {};
+    	if (config.debugMode)
+    		Q.longStackSupport = true;
+
+    	//This changes Durandal's default promise from jQuery to Q
+	    system.defer = function (action) {
+	        var deferred = Q.defer();
+	        action.call(deferred, deferred);
+	        var promise = deferred.promise;
+	        deferred.promise = function () {
+	            return promise;
+	        };
+	        return deferred;
+	    };
+
+	    system.deferred = app.deferred = Q.defer;
+	    
+	    system.delay = function (ms) {
+	      return Q.delay(ms);
+	    };
+
+	    app.deferAll = system.deferAll = Q.all;
+	    app.defer = system.defer;
+	    app.delay = system.delay;
+	    app.log = system.log;
+
+	    app.guid = system.guid;
+    };
+
+    return {
+        install: install
+    };
+});
